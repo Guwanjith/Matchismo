2ca7a65766b5cc764d0ca9410abb7d6406995ca7
Guwanjith Tennekoon <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-10-16 16:05:26 +0530

    Started CardMatchingGame

---
+++ Matchismo/CardMatchingGame.h
@@ -0,0 +1,24 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Guwanjith Tennekoon on 10/16/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+#import "Card.h"
+
+@interface CardMatchingGame : NSObject
+
+-(instancetype)initWithCardCount:(NSUInteger)count
+                        usingDeck:(Deck *)deck;
+
+-(void)chooseCardAtIndex:(NSUInteger)index;
+-(Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly)NSInteger score;
+
+
+@end
---
+++ Matchismo/CardMatchingGame.m
@@ -0,0 +1,22 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Guwanjith Tennekoon on 10/16/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@interface CardMatchingGame()
+
+@property (nonatomic,readwrite)NSInteger score;
+@property (nonatomic, strong) NSMutableArray *cards;// of Card;
+
+@end
+
+
+@implementation CardMatchingGame
+
+
+@end
